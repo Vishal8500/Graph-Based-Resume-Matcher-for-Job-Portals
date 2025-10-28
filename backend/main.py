c8df9237f7b940f48851103d58d2454f14e60023
Vishal8500 <vishalmahender8@gmail.com>
2025-10-28 10:24:29 +0530

    merge ontology

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ fs = gridfs.GridFS(db)
 # Neo4j Config
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "12345678")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "vishal2004")
 neo4j_driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
 # Password hashing
